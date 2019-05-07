8cf025150b1d9d3a624735408a3c92c7634f3e65
heatherjp <39212764+heatherjp@users.noreply.github.com>
2019-05-07 12:15:04 -0600

    suppliers

---
+++ Data/Suppliers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string SupName { get; set; }
+
+        public Supplier Clone()
+        {
+            Supplier copy = new Supplier();
+            copy.SupplierId = this.SupplierId;
+            copy.SupName = this.SupName;
+            return copy;
+        }
+    }
+    // make identical copy of Customer
+
+}
